abe3262c0dd0ef0c9adbcea489d4bb713ae32a73
mi-ko-cute <thx-mail@worysolve.com>
2019-11-04 10:06:51 +0900

    指摘を受けて修正

--- src/QuizFetcher.js
+++ src/QuizFetcher.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 class QuizFetcher {
     async fetch() {
-        const result = await axios.get('https://opentdb.com/api.php?amount=10&type=multiple');
-        const quizData = await result.data;
+        const response = await axios.get('https://opentdb.com/api.php?amount=10&type=multiple');
+        const quizData = response.data;
         return quizData;
     }
 }
