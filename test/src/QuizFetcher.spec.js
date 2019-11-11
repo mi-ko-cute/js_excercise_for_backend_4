aa6750e3ad274050cb4d20a91b714f5103eb618a
mi-ko-cute <thx-mail@worysolve.com>
2019-11-11 20:40:54 +0900

    コールバックでのテストモジュール実装

--- test/src/QuizFetcher.spec.js
+++ test/src/QuizFetcher.spec.js
@@ -91,5 +91,56 @@ describe('QuizFetcherのクラス', () => {
 
             });
         });
+
+        describe('【コールバック（done）版】fetchメソッドの戻り値の型チェック', () => {
+            it('resultsプロパティを持つ', (done) => {
+                const quizfetcher = new QuizFetcher();
+                quizfetcher
+                    .fetch()
+                    .then(response => {
+                        const results = response.results;
+
+                        assert.ok(Array.isArray(results));
+                    })
+
+                done();
+            });
+
+            it('results プロパティはは10件データをもつ配列である', (done) => {
+                const quizfetcher = new QuizFetcher();
+                quizfetcher
+                    .fetch()
+                    .then(response => {
+                        const results = response.results;
+
+                        assert.strictEqual(results.length, 10);
+                    })
+                done();
+            });
+
+            it('results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ', (done) => {
+                const quizfetcher = new QuizFetcher();
+                quizfetcher
+                    .fetch()
+                    .then(response => {
+                        const results = response.results;
+
+                        results.forEach((result) => {
+                            assert.strictEqual(typeof result.category, 'string');
+                            assert.strictEqual(typeof result.type, 'string');
+                            assert.strictEqual(typeof result.difficulty, 'string');
+                            assert.strictEqual(typeof result.question, 'string');
+                            assert.strictEqual(typeof result.correct_answer, 'string');
+                            assert.ok(Array.isArray(result.incorrect_answers));
+                            assert.strictEqual(result.incorrect_answers.length, 3);
+                            result.incorrect_answers.forEach((incorrect_answer) => {
+                                assert.strictEqual(typeof incorrect_answer, 'string');
+                            });
+                        })
+                    });
+
+                done();
+            });
+        });
     });
 });
