49d7ff3f2a32c422dfb8351563d6b08556864f57
mi-ko-cute <thx-mail@worysolve.com>
2019-11-12 11:34:05 +0900

    コメント追加

--- test/src/QuizFetcher.spec.js
+++ test/src/QuizFetcher.spec.js
@@ -19,11 +19,17 @@ describe('QuizFetcherのクラス', () => {
 
                 // results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ
                 results.forEach((result) => {
+                    // category プロパティ : 文字列型
                     assert.strictEqual(typeof result.category, 'string');
+                    // type プロパティ : 文字列型
                     assert.strictEqual(typeof result.type, 'string');
+                    // difficulty プロパティ : 文字列型
                     assert.strictEqual(typeof result.difficulty, 'string');
+                    // question プロパティ : 文字列型
                     assert.strictEqual(typeof result.question, 'string');
+                    // correct_answer プロパティ : 文字列型
                     assert.strictEqual(typeof result.correct_answer, 'string');
+                    // incorrect_answers プロパティ : 3件の文字列を含む配列
                     assert.ok(Array.isArray(result.incorrect_answers));
                     assert.strictEqual(result.incorrect_answers.length, 3);
                     result.incorrect_answers.forEach((incorrect_answer) => {
