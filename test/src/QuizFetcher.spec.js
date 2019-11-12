7a071d41c39ee5b0d66510e147cbd1ab433112e7
mi-ko-cute <thx-mail@worysolve.com>
2019-11-12 12:46:12 +0900

    describe構文,コメント削除

--- test/src/QuizFetcher.spec.js
+++ test/src/QuizFetcher.spec.js
@@ -7,34 +7,23 @@ describe('QuizFetcherのクラス', () => {
             assert.strictEqual(typeof QuizFetcher.fetch, 'function');
         });
 
-        describe('【async/await版】fetchメソッドの戻り値の型チェック', () => {
-            it('resultsプロパティを持つ', async () => {
-                const response = await QuizFetcher.fetch();
-                const results = response.results;
+        it('【async/await版】fetchメソッドの戻り値の型チェック', async () => {
+            const response = await QuizFetcher.fetch();
+            const results = response.results;
 
-                assert.ok(Array.isArray(results));
+            assert.ok(Array.isArray(results));
+            assert.strictEqual(results.length, 10);
 
-                // results プロパティはは10件データをもつ配列である
-                assert.strictEqual(results.length, 10);
-
-                // results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ
-                results.forEach((result) => {
-                    // category プロパティ : 文字列型
-                    assert.strictEqual(typeof result.category, 'string');
-                    // type プロパティ : 文字列型
-                    assert.strictEqual(typeof result.type, 'string');
-                    // difficulty プロパティ : 文字列型
-                    assert.strictEqual(typeof result.difficulty, 'string');
-                    // question プロパティ : 文字列型
-                    assert.strictEqual(typeof result.question, 'string');
-                    // correct_answer プロパティ : 文字列型
-                    assert.strictEqual(typeof result.correct_answer, 'string');
-                    // incorrect_answers プロパティ : 3件の文字列を含む配列
-                    assert.ok(Array.isArray(result.incorrect_answers));
-                    assert.strictEqual(result.incorrect_answers.length, 3);
-                    result.incorrect_answers.forEach((incorrect_answer) => {
-                        assert.strictEqual(typeof incorrect_answer, 'string');
-                    });
+            results.forEach((result) => {
+                assert.strictEqual(typeof result.category, 'string');
+                assert.strictEqual(typeof result.type, 'string');
+                assert.strictEqual(typeof result.difficulty, 'string');
+                assert.strictEqual(typeof result.question, 'string');
+                assert.strictEqual(typeof result.correct_answer, 'string');
+                assert.ok(Array.isArray(result.incorrect_answers));
+                assert.strictEqual(result.incorrect_answers.length, 3);
+                result.incorrect_answers.forEach((incorrect_answer) => {
+                    assert.strictEqual(typeof incorrect_answer, 'string');
                 });
             });
         });
